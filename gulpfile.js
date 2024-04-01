9f042a1dfad835f886e9b6ca1cd7c4a4e38fad22
Mustapha-Nkhili <mustapha.developer.23@gmail.com>
2024-04-01 23:30:22 +0000

    Refactor Gulp configuration for improved CSS compilation

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const rename = require('gulp-rename');
 
 const compileStyles = () =>
-  src("./src/**/*.scss").pipe(sass()).pipe(dest("./src"));
+  src("./dist/**/*.scss")
+    .pipe(sass())
+    .pipe(rename("nkhilicss.css"))
+    .pipe(dest("./dist/css/"));
 
-const watchTask = () => watch(["./src/**/*.scss"], compileStyles);
+const watchTask = () => watch(["./dist/**/*.scss"], compileStyles);
 
 exports.default = series(compileStyles, watchTask);
