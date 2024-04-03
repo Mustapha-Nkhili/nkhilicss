ea1b34d1724f5b14690010936c24d33e73c94ba2
Mustapha-Nkhili <mustapha.developer.23@gmail.com>
2024-04-03 12:38:19 +0000

    Reorganize project structure for improved maintainability and scalability

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,11 @@ const sass = require("gulp-sass")(require("sass"));
 const rename = require('gulp-rename');
 
 const compileStyles = () =>
-  src("./dist/**/*.scss")
+  src("./sass/**/*.scss")
     .pipe(sass())
     .pipe(rename("nkhilicss.css"))
     .pipe(dest("./dist/css/"));
 
-const watchTask = () => watch(["./dist/**/*.scss"], compileStyles);
+const watchTask = () => watch(["./sass/**/*.scss"], compileStyles);
 
 exports.default = series(compileStyles, watchTask);
