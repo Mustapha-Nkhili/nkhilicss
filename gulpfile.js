fd1855b8c0b932ca463c47115a0c2bb6a2ea08a1
Mustapha-Nkhili <mustapha.developer.23@gmail.com>
2024-04-03 15:37:51 +0000

    Updated the default task to include the minification task alongside compilation and watch tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,31 @@
-const { src, dest, watch, series } = require("gulp");
+const { src, dest, watch, series, task } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
-const rename = require('gulp-rename');
+const cleanCSS = require("gulp-clean-css");
+const rename = require("gulp-rename");
 
+// Compile Sass to CSS
 const compileStyles = () =>
   src("./sass/**/*.scss")
     .pipe(sass())
     .pipe(rename("nkhilicss.css"))
     .pipe(dest("./dist/css/"));
 
-const watchTask = () => watch(["./sass/**/*.scss"], compileStyles);
+// Minify CSS
+const minifyCSS = () =>
+  src("./dist/css/nkhilicss.css") // Source from compiled CSS
+    .pipe(cleanCSS())
+    .pipe(rename("nkhilicss.min.css"))
+    .pipe(dest("./dist/css/"));
 
-exports.default = series(compileStyles, watchTask);
+// Watch for changes
+const watchTask = () =>
+  watch(["./sass/**/*.scss"], series(compileStyles, minifyCSS));
+
+// Define minify-css task
+task("minify-css", minifyCSS);
+
+// Define compile-css task
+task("compile-css", compileStyles);
+
+// Default task
+exports.default = series(compileStyles, minifyCSS, watchTask);
